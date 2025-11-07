a3642a3ad2046fa7a94c34e664bc82fc2f930692
Lex Potter <alexanderpotter26@gmail.com>
2025-11-07 17:27:04 -0600

    more ooooooom

--- start_vllm_server.sh
+++ start_vllm_server.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 ./stop_vllm_server.sh
 
+# Help PyTorch avoid fragmentation when (re)allocating CUDA memory
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
 python vllm_server.py "$@"
